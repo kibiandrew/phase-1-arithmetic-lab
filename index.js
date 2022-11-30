8eafdef03b87eb4f8e6974dba0b58e236feffeed
Andrew Kibiwott <kibiandrew@gmail.com>
2022-11-30 12:09:17 +0300

    Copmleted assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+
+
+let random=4;
+Math.random()*10;
+
+let num3=10
+let num4=6
+
+let mod =10%6 ;
+
+let max=Math.max(20,3,6,6,5,18);
---
+++ practice.js
@@ -0,0 +1,11 @@
+//let max=Math.max(20,3,6,6,5,18);
+//console.log(max)
+/*let mod =10%6 ;
+console.log(mod)*/
+/*let random=4;
+Math.random()*10;
+console.log(random)*/
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+console.log(multiply)
